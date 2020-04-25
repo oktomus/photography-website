0776d5ed5e41261ec6224acd7caf08063329bb34
Kevin Masson <hi@oktomus.com>
2020-04-25 13:49:04 +0200

    Place all pictures in the same pswp gallery

--- themes/v1/assets/js/pswp-main.js
+++ themes/v1/assets/js/pswp-main.js
@@ -2,14 +2,15 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 
     // parse slide data (url, title, size ...) from DOM elements 
     // (children of gallerySelector)
-    var parseThumbnailElements = function(el) {
-        var thumbElements = el.childNodes,
-            numNodes = thumbElements.length,
-            items = [],
+    var parseThumbnailElements = function() {
+        var items = [],
             figureEl,
             linkEl,
             size,
             item;
+
+        var thumbElements = document.querySelectorAll(gallerySelector);
+        var numNodes = thumbElements.length;
 
         for(var i = 0; i < numNodes; i++) {
 
@@ -47,6 +48,8 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
             items.push(item);
         }
 
+        console.log('items', items);
+
         return items;
     };
 
@@ -71,100 +74,37 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
             return;
         }
 
-        // find index of clicked item by looping through all child nodes
-        // alternatively, you may define index via data- attribute
-        var clickedGallery = clickedListItem.parentNode,
-            childNodes = clickedListItem.parentNode.childNodes,
-            numChildNodes = childNodes.length,
-            nodeIndex = 0,
-            index;
+        console.log(clickedListItem);
 
-        for (var i = 0; i < numChildNodes; i++) {
-            if(childNodes[i].nodeType !== 1) { 
-                continue; 
-            }
-
-            if(childNodes[i] === clickedListItem) {
-                index = nodeIndex;
-                break;
-            }
-            nodeIndex++;
-        }
-
-
+        var index = clickedListItem.getAttribute('data-pswp-uid');
 
         if(index >= 0) {
             // open PhotoSwipe if valid index found
-            openPhotoSwipe( index, clickedGallery );
+            openPhotoSwipe( index );
         }
         return false;
     };
 
-    // parse picture index and gallery index from URL (#&pid=1&gid=2)
-    var photoswipeParseHash = function() {
-        var hash = window.location.hash.substring(1),
-        params = {};
-
-        if(hash.length < 5) {
-            return params;
-        }
-
-        var vars = hash.split('&');
-        for (var i = 0; i < vars.length; i++) {
-            if(!vars[i]) {
-                continue;
-            }
-            var pair = vars[i].split('=');  
-            if(pair.length < 2) {
-                continue;
-            }           
-            params[pair[0]] = pair[1];
-        }
-
-        if(params.gid) {
-            params.gid = parseInt(params.gid, 10);
-        }
-
-        return params;
-    };
-
-    var openPhotoSwipe = function(index, galleryElement, fromURL) {
+    var openPhotoSwipe = function(index) {
         var pswpElement = document.querySelectorAll('.pswp')[0],
             gallery,
             options,
             items;
 
-        items = parseThumbnailElements(galleryElement);
+        items = parseThumbnailElements();
 
         // define options (if needed)
         // https://photoswipe.com/documentation/options.html
         options = {
 
             // define gallery index (for URL)
-            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+            galleryUID: 1,
 
             showAnimationDuration: 0,
             showHideOpacity: true,
         };
 
-        // PhotoSwipe opened from URL
-        if(fromURL) {
-            if(options.galleryPIDs) {
-                // parse real index when custom PIDs are used 
-                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
-                for(var j = 0; j < items.length; j++) {
-                    if(items[j].pid == index) {
-                        options.index = j;
-                        break;
-                    }
-                }
-            } else {
-                // in URL indexes start from 1
-                options.index = parseInt(index, 10) - 1;
-            }
-        } else {
-            options.index = parseInt(index, 10);
-        }
+        options.index = parseInt(index, 10) - 1;
 
         // exit if index not found
         if( isNaN(options.index) ) {
@@ -183,13 +123,7 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
         galleryElements[i].setAttribute('data-pswp-uid', i+1);
         galleryElements[i].onclick = onThumbnailsClick;
     }
-
-    // Parse URL and open gallery if it contains #&pid=3&gid=1
-    var hashData = photoswipeParseHash();
-    if(hashData.pid && hashData.gid) {
-        openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true );
-    }
 };
 
 // execute above function
-initPhotoSwipeFromDOM('.my-pswp-gallery');
+initPhotoSwipeFromDOM('.pswp-item');
